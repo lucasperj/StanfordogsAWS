1030adb8c1e5b7e5dc0c6b3a19e3154b6522b6bd
Lucas Falcao <52680028+lucasperj@users.noreply.github.com>
2021-11-29 00:18:18 -0300

    Project 4 - AWS/Stanfordogs

---
+++ StopModel.py
@@ -0,0 +1,30 @@
+#Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-custom-labels-developer-guide/blob/master/LICENSE-SAMPLECODE.)
+
+import boto3
+import time
+
+
+def stop_model(model_arn):
+
+    client=boto3.client('rekognition')
+
+    print('Stopping model:' + model_arn)
+
+    #Stop the model
+    try:
+        response=client.stop_project_version(ProjectVersionArn=model_arn)
+        status=response['Status']
+        print ('Status: ' + status)
+    except Exception as e:  
+        print(e)  
+
+    print('Done...')
+    
+def main():
+    
+    model_arn='arn:aws:rekognition:us-east-1:610430599118:project/stanfordogs/version/stanfordogs.2021-11-26T21.06.35/1637971595418'
+    stop_model(model_arn)
+
+if __name__ == "__main__":
+    main() 
